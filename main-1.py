ac187a2fa5b05c13bfef3a9e3d44e351a8a8781a
sheikhsakihasan30-bit <sheikhsakihasan3.0@gmail.com>
2026-09-01 09:51:02 +0600

    Update main-1.py

--- main-1.py
+++ main-1.py
@@ -9,7 +9,7 @@ GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 ADMIN_ID = 8345712050
 
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
